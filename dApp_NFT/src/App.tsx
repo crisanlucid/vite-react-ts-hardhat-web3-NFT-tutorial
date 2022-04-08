30fb1a2a52d10bc85d6be821bb96bfad5cfb7c34
crisanlucid <crisan.lucid@yahoo.com>
2022-04-08 18:34:33 +0200

    feat: Enable and config Router App

--- dApp_NFT/src/App.tsx
+++ dApp_NFT/src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Navbar, Services, Transactions, Welcome } from './components';
 import { NftPage } from './pages/Nft';
 
 const App: React.FC = () => {
   return (
-    <>
-      <NftPage />
-      <div>
-        <div className="gradient-bg-welcome">
-          <Navbar />
-          <Welcome />
-        </div>
-        <Services />
-        <Transactions />
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<NftPage />} />
+          <Route path="*" element={<main>Not Found 404!</main>} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 };
 
