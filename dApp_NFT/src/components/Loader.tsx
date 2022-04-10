170a350bba4ab0021f3dcdcc585703dacec8c4f0
crisanlucid <crisan.lucid@yahoo.com>
2022-04-10 11:31:05 +0200

    [feat] Loader Component Styling

--- dApp_NFT/src/components/Loader.tsx
+++ dApp_NFT/src/components/Loader.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Loader: React.FC = () => {
-  return <>Loader</>;
-};
+const Loader: React.FC = () => (
+  <div className="flex justify-center items-center py-3">
+    <div className="animate-spin rounded-full w-32 h-32 border-l-4 border-red-700"></div>
+  </div>
+);
 
 export default Loader;
